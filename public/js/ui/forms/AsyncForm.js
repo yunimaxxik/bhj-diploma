b41ab4be8bd106edc4daa4019c498f4b3fc15707
yunimaxxik <nicxefonn@yandex.ru>
2026-03-29 18:08:16 +0300

    completed asyncform

--- public/js/ui/forms/AsyncForm.js
+++ public/js/ui/forms/AsyncForm.js
@@ -6,44 +6,56 @@
  * для последующей обработки
  * */
 class AsyncForm {
-  /**
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * Сохраняет переданный элемент и регистрирует события
-   * через registerEvents()
-   * */
-  constructor(element) {
+	/**
+	 * Если переданный элемент не существует,
+	 * необходимо выкинуть ошибку.
+	 * Сохраняет переданный элемент и регистрирует события
+	 * через registerEvents()
+	 * */
+	constructor(element) {
+		if (!element) {
+			throw new Error(
+				'Конструктор AsyncForm: элемент не передан или равен null/undefined',
+			);
+		}
+		this.element = element;
+		this.registerEvents();
+	}
 
-  }
+	/**
+	 * Необходимо запретить отправку формы и в момент отправки
+	 * вызывает метод submit()
+	 * */
+	registerEvents() {
+		this.element.addEventListener('submit', (e) => {
+			e.preventDefault();
+			this.submit();
+		});
+	}
 
-  /**
-   * Необходимо запретить отправку формы и в момент отправки
-   * вызывает метод submit()
-   * */
-  registerEvents() {
+	/**
+	 * Преобразует данные формы в объект вида
+	 * {
+	 *  'название поля формы 1': 'значение поля формы 1',
+	 *  'название поля формы 2': 'значение поля формы 2'
+	 * }
+	 * */
+	getData() {
+		const formData = new FormData(this.element);
+		const data = {};
+		for (const [key, value] of formData.entries()) {
+			data[key] = value;
+		}
+		return data;
+	}
 
-  }
+	onSubmit(options) {}
 
-  /**
-   * Преобразует данные формы в объект вида
-   * {
-   *  'название поля формы 1': 'значение поля формы 1',
-   *  'название поля формы 2': 'значение поля формы 2'
-   * }
-   * */
-  getData() {
-
-  }
-
-  onSubmit(options){
-
-  }
-
-  /**
-   * Вызывает метод onSubmit и передаёт туда
-   * данные, полученные из метода getData()
-   * */
-  submit() {
-
-  }
+	/**
+	 * Вызывает метод onSubmit и передаёт туда
+	 * данные, полученные из метода getData()
+	 * */
+	submit() {
+		this.onSubmit(this.getData());
+	}
 }
