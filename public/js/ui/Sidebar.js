c855cd0a9e1a64a25038e685b17930bcb0586a88
yunimaxxik <nicxefonn@yandex.ru>
2026-03-29 17:49:15 +0300

    completed sidebar

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -4,31 +4,35 @@
  * и за кнопки меню
  * */
 class Sidebar {
-  /**
-   * Запускает initAuthLinks и initToggleButton
-   * */
-  static init() {
-    this.initAuthLinks();
-    this.initToggleButton();
-  }
+	/**
+	 * Запускает initAuthLinks и initToggleButton
+	 * */
+	static init() {
+		this.initAuthLinks();
+		this.initToggleButton();
+	}
 
-  /**
-   * Отвечает за скрытие/показа боковой колонки:
-   * переключает два класса для body: sidebar-open и sidebar-collapse
-   * при нажатии на кнопку .sidebar-toggle
-   * */
-  static initToggleButton() {
+	/**
+	 * Отвечает за скрытие/показа боковой колонки:
+	 * переключает два класса для body: sidebar-open и sidebar-collapse
+	 * при нажатии на кнопку .sidebar-toggle
+	 * */
+	static initToggleButton() {
+		document.querySelector('.sidebar-toggle').addEventListener('click', (e) => {
+			e.preventDefault();
+			document.body.classList.toggle('sidebar-open');
+			document.body.classList.toggle('sidebar-collapse');
+		});
+	}
 
-  }
-
-  /**
-   * При нажатии на кнопку входа, показывает окно входа
-   * (через найденное в App.getModal)
-   * При нажатии на кнопку регастрации показывает окно регистрации
-   * При нажатии на кнопку выхода вызывает User.logout и по успешному
-   * выходу устанавливает App.setState( 'init' )
-   * */
-  static initAuthLinks() {
-
+	/**
+	 * При нажатии на кнопку входа, показывает окно входа
+	 * (через найденное в App.getModal)
+	 * При нажатии на кнопку регастрации показывает окно регистрации
+	 * При нажатии на кнопку выхода вызывает User.logout и по успешному
+	 * выходу устанавливает App.setState( 'init' )
+	 * */
+	static initAuthLinks() {
+    
   }
 }
