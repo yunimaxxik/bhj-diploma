0f3f2015bd821a0cc6ec0d3b68b9ccefe3394ed0
yunimaxxik <nicxefonn@yandex.ru>
2026-03-29 18:33:27 +0300

    completed registerform

--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -3,13 +3,23 @@
  * регистрации
  * */
 class RegisterForm extends AsyncForm {
-  /**
-   * Производит регистрацию с помощью User.register
-   * После успешной регистрации устанавливает
-   * состояние App.setState( 'user-logged' )
-   * и закрывает окно, в котором находится форма
-   * */
-  onSubmit(data) {
+	/**
+	 * Производит регистрацию с помощью User.register
+	 * После успешной регистрации устанавливает
+	 * состояние App.setState( 'user-logged' )
+	 * и закрывает окно, в котором находится форма
+	 * */
+	onSubmit(data) {
+		User.register(data, (err, response) => {
+			if (err) {
+				console.error('Ошибка регистрации:', err);
+				this.showError(err);
+				return;
+			}
 
-  }
+			this.element.reset();
+			App.setState('user-logged');
+			this.closeModal();
+		});
+	}
 }
