40e64a697db651ce740b11a95b0d0316430dc055
yunimaxxik <nicxefonn@yandex.ru>
2026-03-31 17:38:06 +0300

    TransactionsWidget completed

--- public/js/ui/widgets/TransactionsWidget.js
+++ public/js/ui/widgets/TransactionsWidget.js
@@ -5,22 +5,32 @@
  * */
 
 class TransactionsWidget {
-  /**
-   * Устанавливает полученный элемент
-   * в свойство element.
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * */
-  constructor( element ) {
-
-  }
-  /**
-   * Регистрирует обработчики нажатия на
-   * кнопки «Новый доход» и «Новый расход».
-   * При нажатии вызывает Modal.open() для
-   * экземпляра окна
-   * */
-  registerEvents() {
-
-  }
+	/**
+	 * Устанавливает полученный элемент
+	 * в свойство element.
+	 * Если переданный элемент не существует,
+	 * необходимо выкинуть ошибку.
+	 * */
+	constructor(element) {
+		this.element = element;
+		registerEvents();
+	}
+	/**
+	 * Регистрирует обработчики нажатия на
+	 * кнопки «Новый доход» и «Новый расход».
+	 * При нажатии вызывает Modal.open() для
+	 * экземпляра окна
+	 * */
+	registerEvents() {
+		document
+			.querySelector('create-income-button')
+			.addEventListener('click', () => {
+				App.getModal('newIncome').open();
+			});
+		document
+			.querySelector('create-expense-button')
+			.addEventListener('click', () => {
+				App.getModal('newExpense').open();
+			});
+	}
 }
